27da9d9284a7da9562289976865cadd76f6ac608
koroglu.ozan <koroglu.ozan@06691718-432a-11de-86e1-e9dff1ea4737>
2009-05-30 16:56:33 +0000

    english language file

---
+++ js/langs/en.js
@@ -0,0 +1,17 @@
+
+alert("in en.js");
+
+LAN_OPERATOR.addIndexes({
+			mark:'Tracepe r Tracking System',
+			error_1:'\u0130\u015Flemde bir hata olu\u015Ftu.',
+			error_2:'Ayn\u0131 isme sahip eleman zaten bulunmaktadır.',	
+			termsOfUseTitle:'Terms of use',		
+			aboutTitle:'About',
+			usersTitle:'Users',
+			searchResultsTitle:'Search Results',
+			returnToUserListLink:'&laquo; Return to user list',
+			loading:'Loading',
+			noMatchFound:'no match found'
+			//about:'',
+			//termsofuse:''
+		});
