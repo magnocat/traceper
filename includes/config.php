0f6cdefaa17bab59baec06536153f8954e457270
ahmetmermerkaya@gmail.com <ahmetmermerkaya@gmail.com@06691718-432a-11de-86e1-e9dff1ea4737>
2009-05-30 10:44:37 +0000

    Some code parameters like google map api, element counts are defined as macros.

--- includes/config.php
+++ includes/config.php
@@ -23,9 +23,10 @@ $dbPassword = "21236161";
 $dbName = "test";
 
 error_reporting(E_ALL); // edit if you know what you do
-/**
- * This is the number of elements in user and search list page  
- */
+
+define("GOOGLE_MAP_API_KEY", "ABQIAAAAEUQFPfeMmwQlu4rVizjq5RTUBQ_8WQnJ0r_AJ0Rg3Y6UmQXNMhTgFS-OpJG3vTTQzF67ve7Li8eh_g");
+
+/* This is the number of elements in user and search list page  */
 define ("ELEMENT_COUNT_IN_LIST_PAGE", 10); 
 /**
  * This is the number of elements when updating user data in background
