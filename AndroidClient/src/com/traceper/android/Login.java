f21b926d9648826388b22ded7bf0c35e691695a5
ahmetmermerkaya <ahmetmermerkaya@06691718-432a-11de-86e1-e9dff1ea4737>
2009-07-12 22:09:16 +0000

    initial commit.

---
+++ AndroidClient/src/com/traceper/android/Login.java
@@ -0,0 +1,359 @@
+package com.traceper.android;
+
+import java.io.UnsupportedEncodingException;
+import java.security.NoSuchAlgorithmException;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.traceper.R;
+import com.traceper.android.interfaces.IAppService;
+import com.traceper.android.services.AppService;
+import com.traceper.android.tools.AeSimpleMD5;
+
+public class Login extends Activity {	
+
+    protected static final int NOT_CONNECTED_TO_SERVICE = 0;
+	protected static final int FILL_BOTH_USERNAME_AND_PASSWORD = 1;
+	private static final int MAKE_SURE_USERNAME_AND_PASSWORD_CORRECT = 2 ;
+	private static final int NOT_CONNECTED_TO_NETWORK = 3;
+	private static final int UNKNOWN_ERROR_OCCURED = 4;
+	private static final int SETTINGS_DIALOG = 5;
+	private static final int HTTP_REQUEST_FAILED = 6;
+	private EditText usernameText;
+    private EditText passwordText;
+    private Button cancelButton;
+    private CheckBox rememberMeCheckBox;
+    private IAppService appManager;
+    public static final int SIGN_UP_ID = Menu.FIRST;
+    public static final int SETTINGS_ID = Menu.FIRST + 1;
+    public static final int EXIT_APP_ID = Menu.FIRST + 2;
+	
+   
+
+   
+    private ServiceConnection mConnection = new ServiceConnection() {
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            // This is called when the connection with the service has been
+            // established, giving us the service object we can use to
+            // interact with the service.  Because we have bound to a explicit
+            // service that we know is running in our own process, we can
+            // cast its IBinder to a concrete class and directly access it.
+            appManager = ((AppService.IMBinder)service).getService();  
+            appManager.setAuthenticationServerAddress(getSharedPreferences(Configuration.PREFERENCES_NAME, 0).getString(Configuration.PREFERENCES_SERVER_INDEX, Configuration.DEFAULT_SERVER_ADRESS));
+           
+            if (appManager.isUserAuthenticated() == true)
+            {
+            	Intent i = new Intent(Login.this, Main.class);																
+				startActivity(i);
+				Login.this.finish();
+            }
+        }
+
+        public void onServiceDisconnected(ComponentName className) {
+            // This is called when the connection with the service has been
+            // unexpectedly disconnected -- that is, its process crashed.
+            // Because it is running in our same process, we should never
+            // see this happen.
+        	appManager = null;
+            Toast.makeText(Login.this, R.string.local_service_stopped,
+                    Toast.LENGTH_SHORT).show();
+        }
+    };
+	
+	
+    
+    
+    /** Called when the activity is first created. */	
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);    
+
+        // Start and bind the  imService 
+    	startService(new Intent(Login.this,  AppService.class));	               
+    	
+        setContentView(R.layout.login_screen);
+        setTitle("Login - " + Configuration.APPLICATION_NAME);
+        
+        Button loginButton = (Button) findViewById(R.id.login);
+        cancelButton = (Button) findViewById(R.id.cancel_login);
+        usernameText = (EditText) findViewById(R.id.userName);
+        passwordText = (EditText) findViewById(R.id.password);   
+        rememberMeCheckBox = (CheckBox) findViewById(R.id.remember_me_checkbox);
+        
+        SharedPreferences preferences = getSharedPreferences(Configuration.PREFERENCES_NAME, 0);
+        usernameText.setText(preferences.getString(Configuration.PREFERENCES_USERNAME, ""));
+        passwordText.setText(preferences.getString(Configuration.PREFERENCES_PASSWORD, ""));
+        rememberMeCheckBox.setChecked(preferences.getBoolean(Configuration.PREFRENCES_REMEMBER_ME_CHECKBOX, false));
+        
+        loginButton.setOnClickListener(new OnClickListener(){
+			public void onClick(View arg0) 
+			{			
+				SharedPreferences.Editor editor = getSharedPreferences(Configuration.PREFERENCES_NAME, 0).edit();
+				editor.putBoolean(Configuration.PREFRENCES_REMEMBER_ME_CHECKBOX, rememberMeCheckBox.isChecked());
+				editor.commit();
+				
+				if (appManager == null) {
+					showDialog(NOT_CONNECTED_TO_SERVICE);
+					return;
+				}
+				else if (appManager.isNetworkConnected() == false)
+				{
+					showDialog(NOT_CONNECTED_TO_NETWORK);					
+				}
+				else if (usernameText.length() > 0 && passwordText.length() > 0)
+				{					
+					Thread loginThread = new Thread(){
+						private Handler handler = new Handler();
+						@Override
+						public void run() {
+							String password = null;
+							try {
+								password = AeSimpleMD5.MD5(passwordText.getText().toString());
+							} catch (NoSuchAlgorithmException e) {
+								e.printStackTrace();
+							} catch (UnsupportedEncodingException e) {
+								e.printStackTrace();
+							}
+							int result = appManager.authenticateUser(usernameText.getText().toString(), password);
+							
+							if (result == IAppService.HTTP_RESPONSE_SUCCESS){
+								SharedPreferences.Editor editor = getSharedPreferences(Configuration.PREFERENCES_NAME, 0).edit();
+								
+								if (rememberMeCheckBox.isChecked() == true) {									
+		                        	editor.putString(Configuration.PREFERENCES_USERNAME, usernameText.getText().toString());
+		                        	editor.putString(Configuration.PREFERENCES_PASSWORD, passwordText.getText().toString());		                        									
+								}
+								else {
+									editor.remove(Configuration.PREFERENCES_USERNAME);
+									editor.remove(Configuration.PREFERENCES_PASSWORD);
+								}
+								editor.commit();	
+								
+								handler.post(new Runnable(){
+									public void run() {										
+										Intent i = new Intent(Login.this, Main.class);												
+										//i.putExtra(FRIEND_LIST, result);						
+										startActivity(i);	
+										Login.this.finish();										
+									}									
+								});											
+							}
+							else if (result == IAppService.HTTP_RESPONSE_ERROR_UNAUTHORIZED_ACCESS) 
+							{
+								// Authentication failed, inform the user								 
+								handler.post(new Runnable(){
+									public void run() {										
+										showDialog(MAKE_SURE_USERNAME_AND_PASSWORD_CORRECT);
+									}});														
+							}
+							else if (result == IAppService.HTTP_REQUEST_FAILED){
+								handler.post(new Runnable(){
+									public void run() {										
+										showDialog(HTTP_REQUEST_FAILED);
+									}});
+							}
+							else {							
+								handler.post(new Runnable(){
+									public void run() {										
+										showDialog(UNKNOWN_ERROR_OCCURED);
+									}});					
+							}							
+						}
+					};
+					loginThread.start();					
+				}
+				else {
+					// Username or Password is not filled, alert the user					 
+					showDialog(FILL_BOTH_USERNAME_AND_PASSWORD);
+				}				
+			}       	
+        });
+        
+        cancelButton.setOnClickListener(new OnClickListener(){
+			public void onClick(View arg0) 
+			{					
+				appManager.exit();
+				finish();				
+			}        	
+        });        
+    }
+    
+    @Override
+    protected Dialog onCreateDialog(int id) 
+    {    	
+    
+    	switch (id) 
+    	{
+    		case NOT_CONNECTED_TO_SERVICE:
+    		
+    			return new AlertDialog.Builder(Login.this)       
+        		.setMessage(R.string.not_connected_to_service)
+        		.setPositiveButton(R.string.OK, new DialogInterface.OnClickListener() {
+        			public void onClick(DialogInterface dialog, int whichButton) {
+        				/* User clicked OK so do some stuff */
+        			}
+        		})        
+        		.create();    			
+    			
+    		case FILL_BOTH_USERNAME_AND_PASSWORD:
+    		
+    			return new AlertDialog.Builder(Login.this)       
+        		.setMessage(R.string.fill_both_username_and_password)
+        		.setPositiveButton(R.string.OK, new DialogInterface.OnClickListener() {
+        			public void onClick(DialogInterface dialog, int whichButton) {
+        				/* User clicked OK so do some stuff */
+        			}
+        		})        
+        		.create();    
+    			
+    		case MAKE_SURE_USERNAME_AND_PASSWORD_CORRECT:
+
+    			return new AlertDialog.Builder(Login.this)       
+        		.setMessage(R.string.make_sure_username_and_password_correct)
+        		.setPositiveButton(R.string.OK, new DialogInterface.OnClickListener() {
+        			public void onClick(DialogInterface dialog, int whichButton) {
+        				/* User clicked OK so do some stuff */
+        			}
+        		})        
+        		.create();  
+    			
+    		case NOT_CONNECTED_TO_NETWORK:
+
+    			return new AlertDialog.Builder(Login.this)       
+        		.setMessage(R.string.not_connected_to_network)
+        		.setPositiveButton(R.string.OK, new DialogInterface.OnClickListener() {
+        			public void onClick(DialogInterface dialog, int whichButton) {
+        				/* User clicked OK so do some stuff */
+        			}
+        		})        
+        		.create(); 
+    			
+    		case UNKNOWN_ERROR_OCCURED:
+
+    			return new AlertDialog.Builder(Login.this)       
+        		.setMessage(R.string.unknown_error_occured)
+        		.setPositiveButton(R.string.OK, new DialogInterface.OnClickListener() {
+        			public void onClick(DialogInterface dialog, int whichButton) {
+        				/* User clicked OK so do some stuff */
+        			}
+        		})        
+        		.create(); 
+    		
+    		case SETTINGS_DIALOG:
+    			
+    			LayoutInflater factory = LayoutInflater.from(this);
+                View textEntryView = factory.inflate(R.layout.server_address_entry_dialog, null);
+                
+                final EditText server_address_edit = (EditText) textEntryView.findViewById(R.id.server_address_edit);
+              
+                server_address_edit.setText(getSharedPreferences(Configuration.PREFERENCES_NAME, 0).
+                									getString(Configuration.PREFERENCES_SERVER_INDEX, Configuration.DEFAULT_SERVER_ADRESS));
+                
+                return new AlertDialog.Builder(Login.this)
+                   // .setIcon(R.drawable.alert_dialog_icon)
+                    .setTitle(R.string.alert_dialog_settings)
+                    .setView(textEntryView)
+                    .setPositiveButton(R.string.alert_dialog_ok, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int whichButton) {
+                        	SharedPreferences.Editor editor = getSharedPreferences(Configuration.PREFERENCES_NAME, 0).edit();
+                        	editor.putString(Configuration.PREFERENCES_SERVER_INDEX, server_address_edit.getText().toString());
+                        	editor.commit();
+                        	appManager.setAuthenticationServerAddress(server_address_edit.getText().toString());
+                        }
+                    })
+                    .setNegativeButton(R.string.alert_dialog_cancel, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int whichButton) {
+                        	server_address_edit.setText(getSharedPreferences(Configuration.PREFERENCES_NAME, 0).
+									getString(Configuration.PREFERENCES_SERVER_INDEX, Configuration.DEFAULT_SERVER_ADRESS));
+                            /* User clicked cancel so do some stuff */
+                        }
+                    })
+                    .create(); 
+    		case HTTP_REQUEST_FAILED:
+    			return new AlertDialog.Builder(Login.this)       
+        		.setMessage(R.string.http_request_failed)
+        		.setPositiveButton(R.string.OK, new DialogInterface.OnClickListener() {
+        			public void onClick(DialogInterface dialog, int whichButton) {
+        				/* User clicked OK so do some stuff */
+        			}
+        		})        
+        		.create(); 
+    		
+    		
+    		default:
+    			return null;
+    	}
+    	
+    	
+    }
+
+	@Override
+	protected void onPause() 
+	{
+		unbindService(mConnection);
+		super.onPause();
+	}
+
+	@Override
+	protected void onResume() 
+	{		
+		bindService(new Intent(Login.this, AppService.class), mConnection , Context.BIND_AUTO_CREATE);
+	    		
+		super.onResume();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {		
+		boolean result = super.onCreateOptionsMenu(menu);
+		/* 
+		 * show sign up menu item if registration is made enabled.
+		 */
+		if (Configuration.REGISTRATION_ENABLED == true) {
+			 menu.add(0, SIGN_UP_ID, 0, R.string.register).setIcon(R.drawable.register);
+		 }
+		 menu.add(0, SETTINGS_ID, 0, R.string.settings).setIcon(R.drawable.settings);
+		 
+		 menu.add(0, EXIT_APP_ID, 0, R.string.exit_application).setIcon(R.drawable.exit);
+		 
+		return result;
+	}
+	
+	@Override
+	public boolean onMenuItemSelected(int featureId, MenuItem item) {
+	    
+		switch(item.getItemId()) 
+	    {
+	    	case SIGN_UP_ID:
+	    		Intent i = new Intent(Login.this, Register.class);
+	    		startActivity(i);
+	    		return true;
+	    	case SETTINGS_ID:
+	    		showDialog(SETTINGS_DIALOG);
+	    		break;
+	    	case EXIT_APP_ID:
+	    		cancelButton.performClick();
+	    		return true;
+	    }
+	       
+	    return super.onMenuItemSelected(featureId, item);
+	}    
+}
---
+++ AndroidClient/src/com/traceper/android/Main.java
@@ -0,0 +1,148 @@
+package com.traceper.android;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.traceper.R;
+import com.traceper.android.interfaces.IAppService;
+import com.traceper.android.services.AppService;
+
+public class Main extends Activity 
+{
+	private static final int ADD_NEW_FRIEND_ID = Menu.FIRST;
+	private static final int EXIT_APP_ID = Menu.FIRST + 1;
+	private IAppService appService = null;
+	private TextView lastDataSentTimeText;
+
+	public class MessageReceiver extends  BroadcastReceiver  {
+		public void onReceive(Context context, Intent intent) {
+			
+			Log.i("Broadcast receiver ", "received a message");
+			Bundle extra = intent.getExtras();
+			if (extra != null)
+			{
+				String action = intent.getAction();
+				if (action.equals(IAppService.LAST_LOCATION_DATA_SENT_TIME))
+				{
+					Long time = (Long) extra.getLong(IAppService.LAST_LOCATION_DATA_SENT_TIME);
+					lastDataSentTimeText.setText(getFormattedDate(time));
+				}
+			}
+		}
+
+	};
+	public MessageReceiver messageReceiver = new MessageReceiver();
+
+	private ServiceConnection mConnection = new ServiceConnection() 
+	{
+		public void onServiceConnected(ComponentName className, IBinder service) {          
+			appService = ((AppService.IMBinder)service).getService();    
+			
+			if (appService.isUserAuthenticated()) {				
+				setTitle(appService.getUsername() + " - " + Configuration.APPLICATION_NAME);	
+				lastDataSentTimeText.setText(getFormattedDate(appService.getLastLocationSentTime()));
+			}
+			else {
+				Intent i = new Intent(Main.this, Login.class);																
+				startActivity(i);
+				Main.this.finish();
+			}
+		}
+		public void onServiceDisconnected(ComponentName className) {          
+			appService = null;
+			Toast.makeText(Main.this, R.string.local_service_stopped,
+					Toast.LENGTH_SHORT).show();
+		}
+	};
+	
+	
+
+
+	protected void onCreate(Bundle savedInstanceState) 
+	{		
+		super.onCreate(savedInstanceState);   
+		setContentView(R.layout.main);
+		
+		lastDataSentTimeText = (TextView)findViewById(R.id.lastLocationDataSentAtTime);
+
+    }
+
+	@Override
+	protected void onPause() 
+	{
+		unregisterReceiver(messageReceiver);		
+		unbindService(mConnection);
+		super.onPause();
+	}
+
+	@Override
+	protected void onResume() 
+	{			
+		super.onResume();
+		bindService(new Intent(Main.this, AppService.class), mConnection , Context.BIND_AUTO_CREATE);
+		IntentFilter i = new IntentFilter();
+		i.addAction(IAppService.LAST_LOCATION_DATA_SENT_TIME);	
+		
+		//i.addAction(IMService.FRIEND_LIST_UPDATED);
+		registerReceiver(messageReceiver, i);	
+	}
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {		
+		boolean result = super.onCreateOptionsMenu(menu);		
+
+		
+		menu.add(0, EXIT_APP_ID, 0, R.string.exit_application).setIcon(R.drawable.exit);		
+		
+		return result;
+	}
+
+	@Override
+	public boolean onMenuItemSelected(int featureId, MenuItem item) 
+	{		
+
+		switch(item.getItemId()) 
+		{	  
+			case ADD_NEW_FRIEND_ID:
+			{
+			//	Intent i = new Intent(FriendList.this, AddFriend.class);
+			//	startActivity(i);
+				return true;
+			}		
+			case EXIT_APP_ID:
+			{
+				appService.exit();
+				finish();
+				return true;
+			}			
+		}
+
+		return super.onMenuItemSelected(featureId, item);		
+	}	
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {		
+		super.onActivityResult(requestCode, resultCode, data);		
+	}
+	
+	public String getFormattedDate(Long time) 
+	{
+		DateFormat df = new SimpleDateFormat("dd.MM.yyyy HH:mm");
+		return df.format(new Date(time));
+	}
+}
