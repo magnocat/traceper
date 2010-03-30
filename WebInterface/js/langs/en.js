261d918692e3468b29b1ed73d77c5adad8acdcc5
ahmetmermerkaya <ahmetmermerkaya@06691718-432a-11de-86e1-e9dff1ea4737>
2010-03-30 09:17:32 +0000

    Algorithm when getting user's location data is improved. Zoom and max zoom features are added. Drawing trace lines of users capability is added.

--- WebInterface/js/langs/en.js
+++ WebInterface/js/langs/en.js
@@ -14,5 +14,12 @@ LAN_OPERATOR.addIndexes({
 			longitude:'Longitude',
 			searchStringIsTooShort:'Please write a longer word to search',			
 			aboutus:'<div class=\"logo\"></div>traceper is a GPS tracking system via mobile phones, it is free, it is open source, it is simple and it does not require any GPS tracking device, it only uses mobile phones and we think that all mobile phones will support GPS in the near future.<br/><br/><div class=\"title\">Support</div>If you need support to modify and use this software, We can share all information we have, so feel free to contact us.<br/><br/><div class=\"title\">License</div>This software is free. It can be modified and distributed without notification.<br/><br/><div class=\"title\">Disclaimer</div>This software guarantees nothing, use it with your own risk. No responsilibity is taken for any situation.<br/><br/><div class=\"title\">Project Team</div>Ozan Koroglu - koroglu.ozan@gmail.com <br/>Ahmet Oguz Mermerkaya (mekya) - ahmetmermerkaya@gmail.com',
-			termsofuse:'termsofuse'
+			termsofuse:'termsofuse',
+			zoom:'Zoom',
+			zoomMax:'Max zoom',
+			drawTraceLine:'Draw trace lines',
+			wasHere:'was here.',
+			previousPoint:'<< Previous point',
+			nextPoint:'Next point >>',
+			noMorePastDataAvailable:'There is no more past point data available for this user'
 		});
--- WebInterface/js/langs/tr.js
+++ WebInterface/js/langs/tr.js
@@ -14,5 +14,11 @@ LAN_OPERATOR.addIndexes({
 			longitude:'Boylam',
 			searchStringIsTooShort:'L\u00FCtfen aranmas\u0131 i\u00E7in daha uzun kelime giriniz.',
 			aboutus:'<div class=\"logo\"></div>traceper cep telefonlarını kullanan bir GPS takip sistemidir, traceper ücretsiz, açık kaynak kodlu ve kullanımı basit bir yazılım olup çalışması için herhangi bir GPS takip aracına ihtiyaçı yoktur, sadece gps destekleyen cep telefonlarına ihtiyaç duyar ve biz de yakın gelecekte tüm cep telefonlarınının GPS destekli olacağını düşünüyoruz.<br/><br/><div class=\"title\">Destek</div>Bu yazılımı kullanmak veya değiştirmek isterseniz sahip olduğumuz tüm bilgiyi paylaşmaya hazırız, bu yüzden lütfen iletişime geçmek için tereddüt yaşamayınız.<br/><br/><div class=\"title\">Lisans</div>Bu yazılım ücretsizdir, haber vermeden değiştirilebilir veya dağıtılabilir.<br/><br/><div class=\"title\">Tekzip</div>Bu yazılım hiç bir şeyi garanti etmez, yazılımı kullanırken bütün sorumluluk kullanana aittir. Herhangi bir durumda kesinlikle sorumluluk kabul edilmez.<br/><br/><div class=\"title\">Proje Ekibi</div>Ozan Köroğlu - koroglu.ozan@gmail.com <br/>Ahmet Oğuz Mermerkaya (mekya) - ahmetmermerkaya@gmail.com',
-
+			zoom:'Yakınlaştır',
+			zoomMax:'En yakın',
+			drawTraceLine:'İz doğrularını çiz',
+			wasHere:'burdaydı.',
+			previousPoint:'<< Önceki nokta',
+			nextPoint:'Sonraki nokta >>',
+			noMorePastDataAvailable:'Bu kullan\u0131c\u0131 i\u00E7in daha eski bulunduğu noktalar veritabanında mevcut değildir'
 		});
