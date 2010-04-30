2790e020e0e6019424584b819d8538f919880314
ahmetmermerkaya <ahmetmermerkaya@06691718-432a-11de-86e1-e9dff1ea4737>
2010-04-30 09:08:50 +0000

    Photo upload ability is added to traceper

--- WebInterface/js/langs/tr.js
+++ WebInterface/js/langs/tr.js
@@ -5,6 +5,7 @@ LAN_OPERATOR.addIndexes({
 			usersTitle:'Kullan\u0131c\u0131lar',
 			searchResultsTitle:'Arama Sonu\u00E7lar\u0131',
 			returnToUserListLink:'&laquo; Kullan\u0131c\u0131 listesine d\u00F6n',
+			returnToPhotoListLink:'&laquo; Resimler listesine d\u00F6n',
 //			loading:'Y\u00FCkleniyor',
 			noMatchFound:'Uygun sonu\u00E7 bulunamad\u0131',
 			realname:'Ad',
@@ -13,7 +14,7 @@ LAN_OPERATOR.addIndexes({
 			latitude:'Enlem',
 			longitude:'Boylam',
 			searchStringIsTooShort:'L\u00FCtfen aranmas\u0131 i\u00E7in daha uzun kelime giriniz.',
-			aboutus:'<div class=\"logo\"></div>traceper cep telefonlarını kullanan bir GPS takip sistemidir, traceper ücretsiz, açık kaynak kodlu ve kullanımı basit bir yazılım olup çalışması için herhangi bir GPS takip aracına ihtiyaçı yoktur, sadece gps destekleyen cep telefonlarına ihtiyaç duyar ve biz de yakın gelecekte tüm cep telefonlarınının GPS destekli olacağını düşünüyoruz.<br/><br/><div class=\"title\">Destek</div>Bu yazılımı kullanmak veya değiştirmek isterseniz sahip olduğumuz tüm bilgiyi paylaşmaya hazırız, bu yüzden lütfen iletişime geçmek için tereddüt yaşamayınız.<br/><br/><div class=\"title\">Lisans</div>Bu yazılım ücretsizdir, haber vermeden değiştirilebilir veya dağıtılabilir.<br/><br/><div class=\"title\">Tekzip</div>Bu yazılım hiç bir şeyi garanti etmez, yazılımı kullanırken bütün sorumluluk kullanana aittir. Herhangi bir durumda kesinlikle sorumluluk kabul edilmez.<br/><br/><div class=\"title\">Proje Ekibi</div>Ozan Köroğlu - koroglu.ozan@gmail.com <br/>Ahmet Oğuz Mermerkaya (mekya) - ahmetmermerkaya@gmail.com',
+			aboutus:'<div class=\"logo\"></div>traceper cep telefonlarını kullanan bir GPS takip sistemidir, traceper ücretsiz, açık kaynak kodlu ve kullanımı basit bir yazılım olup çalışması için herhangi bir GPS takip aracına ihtiyaçı yoktur, sadece gps destekleyen cep telefonlarına ihtiyaç duyar ve biz de yakın gelecekte tüm cep telefonlarınının GPS destekli olacağını düşünüyoruz.<br/><br/><div class=\"title\">Destek</div>Bu yazılımı kullanmak veya değiştirmek isterseniz sahip olduğumuz tüm bilgiyi paylaşmaya hazırız, bu yüzden lütfen iletişime geçmek için tereddüt yaşamayınız.<br/><br/><div class=\"title\">Lisans</div>Bu yazılım ücretsizdir, haber vermeden değiştirilebilir veya dağıtılabilir.<br/><br/><div class=\"title\">Tekzip</div>Bu yazılım hiç bir şeyi garanti etmez, yazılımı kullanırken bütün sorumluluk kullanana aittir. Herhangi bir durumda kesinlikle sorumluluk kabul edilmez.<br/><br/><div class=\"title\">Proje Ekibi</div>Ozan Köroğlu - koroglu.ozan@gmail.com <br/>Ahmet Oğuz Mermerkaya - ahmetmermerkaya@gmail.com',
 			zoom:'Yakınlaştır',
 			zoomMax:'En yakın',
 			drawTraceLine:'İz doğrularını çiz',
@@ -21,5 +22,12 @@ LAN_OPERATOR.addIndexes({
 			previousPoint:'<< Önceki nokta',
 			nextPoint:'Sonraki nokta >>',
 			noMorePastDataAvailable:'Bu kullan\u0131c\u0131 i\u00E7in daha eski bulunduğu noktalar veritabanında mevcut değildir',
-			clearTraceLines:'Doğruları temizle'
+			clearTraceLines:'Doğruları temizle',
+			operations:'İşlemler',
+			photosTitle:'Resimler',
+			photosSearchBox:'Yükleyene göre resim ara',
+			usersSearchBox:'Kullanıcı ara',
+			next:'Sonraki >',
+			previous:'< Önceki',
+			uploader:'Yükleyen'
 		});
