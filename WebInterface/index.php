b209c4b1d9372430cdb9fb5b0555d6a4389eb4df
ahmetmermerkaya <ahmetmermerkaya@06691718-432a-11de-86e1-e9dff1ea4737>
2010-04-30 09:10:11 +0000

    Photo upload ability is added to traceper

--- WebInterface/index.php
+++ WebInterface/index.php
@@ -9,37 +9,46 @@
 *********************************************/
 define("IN_PHP", true);
 
+define("WEB_ADDRESS", 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['SCRIPT_NAME']));
+
 require_once("includes/config.php");
 require_once("includes/content.php");
 
 $dbc = NULL;  // database connectivity;
 $out = NULL;
 
-if (isset($_POST['action']) && !empty($_POST['action'])) 
+if (isset($_REQUEST['action']) && !empty($_REQUEST['action'])) 
 {
-	$action = $_POST['action'];	
+	$action = $_REQUEST['action'];	
 	if (strpos($action, WEB_CLIENT_ACTION_PREFIX) === 0)
 	{
-		require_once('classes/WebClientManager.php');
+//		require_once('classes/WebClientManager.php');
 		$dbc = getMySQLOperator($dbc, $dbHost,$dbUsername,$dbPassword,$dbName);
-		$wcm = new WebClientManager($dbc, WEB_CLIENT_ACTION_PREFIX, STAFF_TRACKER_TABLE_PREFIX, ELEMENT_COUNT_IN_LIST_PAGE, ELEMENT_COUNT_IN_LOCATIONS_PAGE);
+		$wcm = new WebClientManager($dbc, WEB_CLIENT_ACTION_PREFIX, STAFF_TRACKER_TABLE_PREFIX, 
+									ELEMENT_COUNT_IN_LIST_PAGE, ELEMENT_COUNT_IN_LOCATIONS_PAGE,
+									ELEMENT_COUNT_IN_PHOTO_PAGE);
 		session_start();
 		if (!isset($_SESSION["dataFetchedTime"])){
 			$_SESSION["dataFetchedTime"] = time();
 		}
-		$out = $wcm->process($_POST, &$_SESSION["dataFetchedTime"]);
+		if (!isset($_SESSION["imageFetchedTime"])){
+			$_SESSION["imageFetchedTime"] = time();
+		}
+		$wcm->setImageRelatedVars(UPLOAD_DIRECTORY, MISSING_IMAGE, IMAGE_HANDLER);
+		$out = $wcm->process($_REQUEST, &$_SESSION["dataFetchedTime"], &$_SESSION["imageFetchedTime"]);
 	}
 	else if (strpos($action, DEVICE_ACTION_PREFIX) === 0)
 	{
-		require_once ('classes/DeviceManager.php');
+//		require_once ('classes/DeviceManager.php');
 		$dbc = getMySQLOperator($dbc, $dbHost,$dbUsername,$dbPassword,$dbName);
 		$dm = new DeviceManager($dbc, DEVICE_ACTION_PREFIX, STAFF_TRACKER_TABLE_PREFIX, 
 								GPS_MIN_DATA_SENT_INTERVAL, GPS_MIN_DISTANCE_INTERVAL);
-		$out = $dm->process($_POST);
+		$dm->setUploadPath(UPLOAD_DIRECTORY);
+		$out = $dm->process($_REQUEST);
 	}
 }
 else {	
-	$out = getContent($_SERVER['PHP_SELF'], UPDATE_USER_LIST_INTERVAL, QUERYING_UPDATED_USER_LIST_INTERVAL, GOOGLE_MAP_API_KEY, LANGUAGE);	
+	$out = getContent($_SERVER['PHP_SELF'], FETCH_PHOTOS_IN_INITIALIZATION, UPDATE_USER_LIST_INTERVAL, QUERYING_UPDATED_USER_LIST_INTERVAL, GOOGLE_MAP_API_KEY, LANGUAGE);	
 }
 
 
@@ -57,5 +66,8 @@ function getMySQLOperator($dbc, $dbHost,$dbUsername,$dbPassword,$dbName ){
 		$dbc = new MySQLOperator($dbHost,$dbUsername,$dbPassword,$dbName);
 	}
 	return $dbc;
+}
+function __autoload($class_name) {
+    require_once dirname(__FILE__) .'/classes/' . $class_name . '.php';
 }
 ?>
