4ead01f08237ea5f2baec4338d0eccc196c32b04
ahmetmermerkaya <ahmetmermerkaya@06691718-432a-11de-86e1-e9dff1ea4737>
2011-09-28 03:29:02 +0000

    Branches are mixed. It is reverted to previous version.

--- branches/DevAndroidClient/src/com/traceper/android/interfaces/IAppService.java
+++ branches/DevAndroidClient/src/com/traceper/android/interfaces/IAppService.java
@@ -1,27 +1,8 @@
 package com.traceper.android.interfaces;
 
-import java.util.ArrayList;
-
 
 
 public interface IAppService {
-	
-	public class CarOptions {
-		private int id;
-		private String title;
-		public CarOptions(int id, String title) {
-			super();
-			this.id = id;
-			this.title = title;
-		}
-		public long getId() {
-			return this.id;
-		}
-		public CharSequence getTitle() {
-			return this.title;
-		}
-		
-	}
 	
 	public static final int HTTP_REQUEST_FAILED = 0;	
 	//protocol defined statics
@@ -32,9 +13,6 @@ public interface IAppService {
 	public static final int HTTP_RESPONSE_ERROR_UNAUTHORIZED_ACCESS = -4;
 	public static final int HTTP_RESPONSE_ERROR_EMAIL_EXISTS = -5;
 	public static final int HTTP_RESPONSE_ERROR_EMAIL_NOT_VALID = -9;
-	public static final int HTTP_RESPONSE_ERROR_USER_NOT_VALID = -10;
-	public static final int HTTP_RESPONSE_ERROR_USER_ACCOUNT_EXPIRED = -11;
-	public static final int HTTP_RESPONSE_ERROR_CAR_IN_USE = -12;
 	//self-defined error	
 	public static final int HTTP_RESPONSE_ERROR_UNKNOWN_RESPONSE = -100;
 
@@ -61,15 +39,5 @@ public interface IAppService {
 	public void setAuthenticationServerAddress(String address);
 	
 	public int sendImage(byte[] image, boolean publicData);
-	
-	public ArrayList<CarOptions> getCarOptions();
-
-	public int sendCarServices(long id);
-	
-	public int authenticateCar(String carname, String password);
-
-	public int setUserStatus(boolean userOnline);
-
-	public int setUserWithInDistance(String distance);
 	
 }
