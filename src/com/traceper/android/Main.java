ccc274bceff93fac6a07766a22600f2eeed0c74b
ahmetmermerkaya <ahmetmermerkaya@06691718-432a-11de-86e1-e9dff1ea4737>
2011-10-09 11:43:02 +0000

    autoCheckBox click operation is deleted and its functionality has been moved the setOnCheckedChange listener...

--- src/com/traceper/android/Main.java
+++ src/com/traceper/android/Main.java
@@ -34,21 +34,21 @@ import com.traceper.android.services.AppService;
 
 public class Main extends Activity 
 {
-	
-	
-	
+
+
+
 	private static final int TAKE_PICTURE_ID = Menu.FIRST;
 	private static final int EXIT_APP_ID = Menu.FIRST + 1;
 	private IAppService appService = null;
 	private TextView lastDataSentTimeText;
 	private Button takePhoto;
-	private CheckBox AutoCheckbox; 
+	private CheckBox autoSendLocationCheckbox; 
 	private Handler handler = new Handler();
-	
-	
+
+
 	public class MessageReceiver extends  BroadcastReceiver  {
 		public void onReceive(Context context, Intent intent) {
-			
+
 			Log.i("Broadcast receiver ", "received a message");
 			Bundle extra = intent.getExtras();
 			if (extra != null)
@@ -69,7 +69,7 @@ public class Main extends Activity
 	{
 		public void onServiceConnected(ComponentName className, IBinder service) {          
 			appService = ((AppService.IMBinder)service).getService();    
-			
+
 			if (appService.isUserAuthenticated()) {				
 				setTitle(appService.getUsername() + " - " + Configuration.APPLICATION_NAME);	
 				Long dt = appService.getLastLocationSentTime();
@@ -89,23 +89,20 @@ public class Main extends Activity
 					Toast.LENGTH_SHORT).show();
 		}
 	};
-	
-	
+
+
 
 
 	protected void onCreate(Bundle savedInstanceState) 
 	{		
 		super.onCreate(savedInstanceState);   
 		setContentView(R.layout.main);
-		AutoCheckbox = (CheckBox) findViewById(R.id.auto_check);
+		autoSendLocationCheckbox = (CheckBox) findViewById(R.id.auto_check);
 		takePhoto = (Button) findViewById(R.id.take_upload_photo_button);
 		takePhoto.setCompoundDrawablesWithIntrinsicBounds(android.R.drawable.ic_menu_camera,0,0,0);
 		SharedPreferences preferences = getSharedPreferences(Configuration.PREFERENCES_NAME, 0);
-		AutoCheckbox.setChecked(preferences.getBoolean(Configuration.PREFRENCES_AUTO_SEND_CHECKBOX, false));
-		
-		
-	
-		
+		autoSendLocationCheckbox.setChecked(preferences.getBoolean(Configuration.PREFRENCES_AUTO_SEND_CHECKBOX, false));
+
 		takePhoto.setOnClickListener(new OnClickListener() {
 			public void onClick(View arg0) {
 				Intent i = new Intent(Main.this, CameraController.class);
@@ -114,50 +111,33 @@ public class Main extends Activity
 		});
 		lastDataSentTimeText = (TextView)findViewById(R.id.lastLocationDataSentAtTime);
 
-		AutoCheckbox.setOnClickListener(new OnClickListener() {
-            public void onClick(View v) {
-            	SharedPreferences.Editor editor = getSharedPreferences(Configuration.PREFERENCES_NAME,0).edit();
-            	editor.putBoolean(Configuration.PREFRENCES_AUTO_SEND_CHECKBOX,AutoCheckbox.isChecked());
-            	editor.commit();
-            	
-            	           	
-                  if (((CheckBox) v).isChecked()) {
-                	appService.setAutoCheckin(true);
-                } else {
-                	appService.setAutoCheckin(false);
-                }
-            }
-        });
-		
-	
-		
+		autoSendLocationCheckbox.setOnCheckedChangeListener(new OnCheckedChangeListener() { 
 
-		AutoCheckbox.setOnCheckedChangeListener(new OnCheckedChangeListener() { 
+			@Override 
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) { 
+				SharedPreferences.Editor editor = getSharedPreferences(Configuration.PREFERENCES_NAME,0).edit();
+				editor.putBoolean(Configuration.PREFRENCES_AUTO_SEND_CHECKBOX,isChecked);
+				editor.commit();
+				if (isChecked) { 
+					appService.setAutoCheckin(true);
+					Toast.makeText(getBaseContext(), "Checked", Toast.LENGTH_SHORT).show(); 
+				} 
+				else 
+				{ 
+					appService.setAutoCheckin(false);
+					Toast.makeText(getBaseContext(), "UnChecked", Toast.LENGTH_SHORT).show(); 
+				} 
 
-		@Override 
-		public void onCheckedChanged(CompoundButton buttonView, 
-		boolean isChecked) { 
-		// TODO Auto-generated method stub 
-		if (buttonView.isChecked()) { 
-		Toast.makeText(getBaseContext(), "Checked", 
-		Toast.LENGTH_SHORT).show(); 
-		} 
-		else 
-		{ 
-		Toast.makeText(getBaseContext(), "UnChecked", 
-		Toast.LENGTH_SHORT).show(); 
-		} 
-
-		} 
+			} 
 		}); 
 
-		
-	
-		
-    }
 
-	
-	
+
+
+	}
+
+
+
 	@Override
 	protected void onPause() 
 	{
@@ -173,58 +153,58 @@ public class Main extends Activity
 		bindService(new Intent(Main.this, AppService.class), mConnection , Context.BIND_AUTO_CREATE);
 		IntentFilter i = new IntentFilter();
 		i.addAction(IAppService.LAST_LOCATION_DATA_SENT_TIME);	
-		
+
 		//i.addAction(IMService.FRIEND_LIST_UPDATED);
 		registerReceiver(messageReceiver, i);	
-		
-	
-		
-		
+
+
+
+
 	}
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {		
 		boolean result = super.onCreateOptionsMenu(menu);		
 
-//		menu.add(0, TAKE_PICTURE_ID, 0, R.string.take_photo).setIcon(android.R.drawable.ic_menu_camera);
-		
-		
+		//		menu.add(0, TAKE_PICTURE_ID, 0, R.string.take_photo).setIcon(android.R.drawable.ic_menu_camera);
+
+
 		menu.add(0, EXIT_APP_ID, 0, R.string.exit_application).setIcon(R.drawable.exit);	
-		
+
 		return result;
 	}
 
 
-	
-	
+
+
 	@Override
 	public boolean onMenuItemSelected(int featureId, MenuItem item) 
 	{		
 
 		switch(item.getItemId()) 
 		{	  
-			case TAKE_PICTURE_ID:
-			{
-				Intent i = new Intent(Main.this, CameraController.class);
-				startActivity(i);
-				return true;
-			}		
-			case EXIT_APP_ID:
-			{
-				appService.exit();
-				finish();
-				return true;
-			}			
+		case TAKE_PICTURE_ID:
+		{
+			Intent i = new Intent(Main.this, CameraController.class);
+			startActivity(i);
+			return true;
+		}		
+		case EXIT_APP_ID:
+		{
+			appService.exit();
+			finish();
+			return true;
+		}			
 		}
 
 		return super.onMenuItemSelected(featureId, item);		
 	}	
-	
+
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {		
 		super.onActivityResult(requestCode, resultCode, data);		
 	}
-	
+
 	public String getFormattedDate(Long time) 
 	{
 		DateFormat df = new SimpleDateFormat("dd MMM yyyy HH:mm");
