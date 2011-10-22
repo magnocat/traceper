82dfba8a68960173fde9b258498354847d2f681d
adnankalay@gmail.com <adnankalay@gmail.com@06691718-432a-11de-86e1-e9dff1ea4737>
2011-10-22 19:48:39 +0000

    Migration of Change Password Form to Yii Framework

--- branches/DevWebInterface/yii/protected/models/ChangePasswordForm.php
+++ branches/DevWebInterface/yii/protected/models/ChangePasswordForm.php
@@ -11,8 +11,6 @@ class ChangePasswordForm extends CFormModel
 	public $newPassword;
 	public $newPasswordAgain;
 
-	private $_identity;
-
 	/**
 	 * Declares the validation rules.
 	 * The rules state that username and password are required,
@@ -22,9 +20,16 @@ class ChangePasswordForm extends CFormModel
 	{
 		return array(
 			// username and password are required
-			array('currentPassword, newPassword, newPasswordAgain', 'required'),
+			array('currentPassword, newPassword, newPasswordAgain', 'required',
+			'message'=>'Field cannot be blank!'),
+			// current password needs to be checked
+			array('currentPassword', 'checkCurrentPassword'),			
 			// password needs to be authenticated
-			array('newPassword', 'compare', 'compareAttribute'=>'newPasswordAgain'),
+			array('newPasswordAgain', 'compare', 'compareAttribute'=>'newPassword',
+			'message'=>'Passwords not same!'),
+			
+			array('newPassword', 'compare', 'compareAttribute'=>'currentPassword',
+				  'operator'=>'!=',	'message'=>'Same with current password!'),
 		);
 	}
 
@@ -34,9 +39,25 @@ class ChangePasswordForm extends CFormModel
 	public function attributeLabels()
 	{
 		return array(
-			'currentPassword'=>Yii::t('general', 'Current Password:'),
-			'newPassword'=>Yii::t('general', 'New Password:'),
-			'newPasswordAgain'=>Yii::t('general', 'New Password(again):'),
+			'currentPassword'=>Yii::t('general', 'Current Password'),
+			'newPassword'=>Yii::t('general', 'New Password'),
+			'newPasswordAgain'=>Yii::t('general', 'New Password(again)'),
 		);
 	}
+	
+	/**
+	 * Checks the current password.
+	 */
+	public function checkCurrentPassword($attribute,$params)
+	{
+		if(!$this->hasErrors())
+		{						
+			$result = Users::model()->findByPk(Yii::app()->user->id,'password=:password', array(':password'=> md5($this->currentPassword)));
+			
+			if($result == NULL)
+			{
+				$this->addError('currentPassword','Password incorrect!');	
+			}
+		}
+	}	
 }
