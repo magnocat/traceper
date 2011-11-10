a33a1d9c8f80a9ef28172bf12968cd991928be38
Salman.Murat@gmail.com <Salman.Murat@gmail.com@06691718-432a-11de-86e1-e9dff1ea4737>
2011-11-10 18:55:57 +0000

    traceper_geofence table is added. traceper.sql is updated. Geofence.php is generated by using yii framework. actionCreateGeofence() is added to UsersController.php for saving geofence points to database.

---
+++ GeoFenceOperator.php
@@ -0,0 +1,58 @@
+<?php 
+
+class GeoFenceOperator {
+	
+	/*
+	 * geofence[i]['latitude']
+	 * geofence[i]['longitude']
+	 * 
+	 * point['latitude'],
+	 * point['longitude'] 
+	 * */
+	public static function  isGeoFenceContainsLocation($geoFence, $point) {
+
+		// Raycast point in polygon method
+		$numPoints = count($geoFence); //MAP_OPERATOR.getPointNumberOfGeoFencePath(geoFence);
+		$inPoly = false;
+		$j = $numPoints-1;
+		
+		for($i = 0; $i < $numPoints; $i++) 
+		{ 
+			if ($geoFence[$i]['longitude'] < $point['longitude'] 
+				&& $geoFence[$j]['longitude'] >= $point['longitude'] 
+				|| $geoFence[$j]['longitude'] < $point['longitude'] 
+				&& $geoFence[$i]['longitude']  >= $point['longitude'])	
+			 {
+				if ($geoFence[$i]['latitude'] + ($point['longitude'] - $geoFence[$i]['longitude']) / ($geoFence[$j]['longitude'] - $geoFence[$i]['longitude']) * ($geoFence[$j]['latitude'] - $geoFence[$i]['latitude']) <$point['latitude']) {
+					$inPoly = !$inPoly;
+				}
+			}
+			$j = $i;
+		}
+		return $inPoly;
+	}
+}
+
+$geoFence = array(
+					array('latitude'=>38.445388,
+						 'longitude'=>-85.341797),
+					array('latitude'=>35.353216,
+						 'longitude'=>-94.438477),
+					array('latitude'=>41.228249,
+						 'longitude'=>-96.031494),
+				);
+$point = array('latitude'=>35.35326,
+				'longitude'=>-94.43846);
+
+if (GeoFenceOperator::isGeoFenceContainsLocation($geoFence, $point))
+{
+	echo "true";
+}
+else 
+{
+	echo "false";
+}
+
+
+
+?>
