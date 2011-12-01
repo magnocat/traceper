e05ef202e87cc7dd7dfb4e934eb1b8efc376b4bb
adnankalay@gmail.com <adnankalay@gmail.com@06691718-432a-11de-86e1-e9dff1ea4737>
2011-12-01 22:59:39 +0000

    User search part migrated into the user tab and results are displayed as a separate form window.

---
+++ branches/DevWebInterface/yii/protected/views/users/usersInfo.php
@@ -0,0 +1,42 @@
+<?php 
+	if (isset($dataProvider)) {
+		$this->renderPartial('userList', array('dataProvider'=>$dataProvider), false, true);
+	}
+	
+	echo '<div id="userSearchResults"></div>';
+?>
+
+<div class="form">
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'id'=>'searchUser-form',
+	'enableClientValidation'=>true,
+	'clientOptions'=> array(
+						'validateOnSubmit'=> true,
+						'validateOnChange'=>true,
+					 ),
+
+	));
+	 ?>
+	 
+	 
+
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'keyword'); ?>
+		<?php echo $form->textField($model,'keyword'); ?>
+		<?php echo CHtml::ajaxSubmitButton('Submit', $this->createUrl('users/search'), 
+											array(
+												'complete'=> 'function() { $("#userSearchResults").dialog("open"); return false;}',
+												'update'=> '#userSearchResults',
+												 ),
+											null);?>
+		<?php echo $form->error($model,'keyword'); 	?>
+	</div>
+
+	<div class="row buttons">
+		<?php  ?>
+												
+	</div>
+
+<?php $this->endWidget(); ?>
+</div>
