c7961374ec7d5f29a93652392dbc760540c063fb
adnankalay@gmail.com <adnankalay@gmail.com@06691718-432a-11de-86e1-e9dff1ea4737>
2011-12-06 08:12:39 +0000

    The elements at the Users tab view displayed as column by coulmn.

--- protected/views/users/_user.php
+++ protected/views/users/_user.php
@@ -1,17 +1,19 @@
 <div>
+<div style='float: left;width:50%'>
 <?php 
 echo CHtml::link($data['realname'], "#", 
 				 array('onclick'=>'TRACKER.trackUser('. $data['id'] .');')); 
 echo "&nbsp;&nbsp;&nbsp;&nbsp;";
+?>
+</div>
 
-$space_length = 10 - strlen($data['realname']);
-
-//echo $space_length;
-
-for ( $i = 0; $i <= $space_length; $i++) {
-	echo "&nbsp;";
-}
-
+<!--//$space_length = 10 - strlen($data['realname']);-->
+<!--//-->
+<!--//for ( $i = 0; $i <= $space_length; $i++) {-->
+<!--//	echo "&nbsp;";-->
+<!--//}-->
+<div style='float: left;'>
+<?php
 echo CHtml::link('<img src="images/delete.png"  />', '#',
 					array('onclick'=>CHtml::ajax(
 											array(
@@ -20,7 +22,9 @@ echo CHtml::link('<img src="images/delete.png"  />', '#',
 											)))
 					  );
 echo "&nbsp;";
-
+?>
+</div>
+<?php
 if (isset($data['status']) && $data['status'] == 0 
 	&& isset($data['requester']) && $data['requester'] == false) 
 {
@@ -28,6 +32,9 @@ if (isset($data['status']) && $data['status'] == 0
 	 * if status is zero, it means friend ship request is made and not yet confirmed.
 	 * requester is about who made first friend request if one is requester he cannot approve friendship
 	 */
+?>
+<div style='float: left;'>
+<?php	
 	echo CHtml::link('<img src="images/approve.png"  />', '#',
 					array('onclick'=>CHtml::ajax(
 											array(
@@ -35,12 +42,17 @@ if (isset($data['status']) && $data['status'] == 0
 												'success'=> 'function(result) { alert(result); }',
 											)))
 					  );
-	
+?>	
+</div>	
+<?php			  					 	
 }	
 else if (isset($data['status']) && $data['status'] == -1) {
 	/*
 	 * if status is not exist or equal to -1 it means there is no relation between these users.
 	 */
+?>
+<div style='float: left;'>
+<?php	
 	echo CHtml::link('<img src="images/user_add_friend.png"  />', '#',
 					  array('onclick'=>CHtml::ajax(
 					  						array('url'=>$this->createUrl('users/addAsFriend', array('friendId'=>$data['id'])),
@@ -49,6 +61,11 @@ else if (isset($data['status']) && $data['status'] == -1) {
 					  					)
 					  		)
 					 );
+?>					 
+</div>	
+<?php				 
 }				  
 ?>
 </div>
+
+</br>
