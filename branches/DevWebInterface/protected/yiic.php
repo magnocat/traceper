5601d796272b909434d816b0e1b27e4d0f5d313d
ahmetmermerkaya <ahmetmermerkaya@06691718-432a-11de-86e1-e9dff1ea4737>
2011-12-25 12:59:52 +0000

    Redundant data in console.php has been removed. yiic.php revert back to original state.

--- branches/DevWebInterface/protected/yiic.php
+++ branches/DevWebInterface/protected/yiic.php
@@ -1,32 +1,8 @@
 <?php
 
 // change the following paths if necessary
-//$yiic=dirname(__FILE__).'/../../../../softwares/yii-1.1.8.r3324/framework/yiic.php';
-$yiic=dirname(__FILE__).'/../yii-1.1.8.r3324/framework/yii.php';
+$yiic=dirname(__FILE__).'/../yii-1.1.8.r3324/framework/yiic.php';
 $config=dirname(__FILE__).'/config/console.php';
 
+
 require_once($yiic);
-
-if(isset($config))
-{
-	//$command_directory=dirname(__FILE__).'/../yii-1.1.8.r3324/framework/yii.php';
-	$app=Yii::createConsoleApplication($config);
-	//$app->commandRunner->addCommands(YII_PATH.'/commands');
-	$app->commandRunner->addCommands(dirname(__FILE__).'/commands');
-	$env=@getenv('YII_CONSOLE_COMMANDS');
-	
-	if(!empty($env))
-	{
-		//echo "tamam";
-		$app->commandRunner->addCommands($env);
-	}
-	/*
-	else
-		echo "eksik";
-	*/
-}
-else
-	//$app=Yii::createConsoleApplication(array('basePath'=>dirname(__FILE__).'/cli'));
-	echo "hata";
-
-$app->run();
