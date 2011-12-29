2b23e3f13e4f10fb67daef1975c94df3861ee1d5
adnankalay@gmail.com <adnankalay@gmail.com@06691718-432a-11de-86e1-e9dff1ea4737>
2011-12-29 22:49:54 +0000

    added for users - groups feature.

---
+++ protected/models/Groups.php
@@ -0,0 +1,88 @@
+<?php
+
+/**
+ * This is the model class for table "traceper_groups".
+ *
+ * The followings are the available columns in table 'traceper_groups':
+ * @property string $Id
+ * @property string $name
+ * @property string $description
+ */
+class Groups extends CActiveRecord
+{
+	/**
+	 * Returns the static model of the specified AR class.
+	 * @return Groups the static model class
+	 */
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return 'traceper_groups';
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+			array('name, description', 'required'),
+			array('name', 'length', 'max'=>20),
+			array('description', 'length', 'max'=>200),
+			// The following rule is used by search().
+			// Please remove those attributes that should not be searched.
+			array('Id, name, description', 'safe', 'on'=>'search'),
+		);
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array();
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'Id' => 'ID',
+			'name' => 'Name',
+			'description' => 'Description',
+		);
+	}
+
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
+	 */
+	public function search()
+	{
+		// Warning: Please modify the following code to remove attributes that
+		// should not be searched.
+
+		$criteria=new CDbCriteria;
+
+		$criteria->compare('Id',$this->Id,true);
+		$criteria->compare('name',$this->name,true);
+		$criteria->compare('description',$this->description,true);
+
+		return new CActiveDataProvider($this, array(
+			'criteria'=>$criteria,
+		));
+	}
+}
