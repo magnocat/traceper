ae932ec23805477965013d9431abea947a068efc
adnankalay@gmail.com <adnankalay@gmail.com@06691718-432a-11de-86e1-e9dff1ea4737>
2012-01-03 21:52:36 +0000

    To create a new group capability added.

---
+++ branches/DevWebInterface/protected/views/groups/createGroup.php
@@ -0,0 +1,73 @@
+<?php 
+$this->beginWidget('zii.widgets.jui.CJuiDialog', array(
+	    'id'=>'createGroupWindow',
+	    // additional javascript options for the dialog plugin
+	    'options'=>array(
+	        'title'=>Yii::t('general', 'Create New Group'),
+	        'autoOpen'=>false,
+	        'modal'=>true, 
+			'resizable'=>false,
+			'width'=> '320px'      
+	    ),
+	));
+?>
+	<div class="form">
+	<?php $form=$this->beginWidget('CActiveForm', array(
+		'id'=>'createGroup-form',
+		'enableClientValidation'=>true,
+		'clientOptions'=> array(
+							'validateOnSubmit'=> true,
+							'validateOnChange'=>false,
+						 ),
+	
+	)); ?>
+	
+
+		<div class="row">
+			<?php echo $form->labelEx($model,'name'); ?>
+			<?php echo $form->textField($model,'name'); ?>
+			<?php $errorMessage = $form->error($model,'name'); 
+				  if (strip_tags($errorMessage) == '') { echo '<div class="errorMessage">&nbsp;</div>'; }
+				  else { echo $errorMessage; }
+			?>			
+		</div>
+	
+		<div class="row">
+			<?php echo $form->labelEx($model,'description'); ?>
+			<?php echo $form->textarea($model,'description', array('rows'=>5, 'cols'=>36,'resizable'=>false)); ?>	
+			<?php $errorMessage = $form->error($model,'description'); 
+				  if (strip_tags($errorMessage) == '') { echo '<div class="errorMessage">&nbsp;</div>'; }
+				  else { echo $errorMessage; }
+			?>
+		</div>
+	
+		<div class="row buttons">
+			<?php echo CHtml::ajaxSubmitButton('Create', $this->createUrl('groups/createGroup'), 
+												array(
+													'success'=> 'function(result){ 
+																	try {
+																		var obj = jQuery.parseJSON(result);
+																		if (obj.result && obj.result == "1") 
+																		{
+																			$("#createGroupWindow").dialog("close");
+																		}
+																	}
+																	catch (error){
+																		$("#createGroupWindow").html(result);
+																	}
+																 }',
+													 ),
+												null); ?>
+												
+			<?php echo CHtml::htmlButton('Cancel',  
+												array(
+													'onclick'=> '$("#createGroupWindow").dialog("close"); return false;',
+													 ),
+												null); ?>												
+		</div>
+	
+	<?php $this->endWidget(); ?>
+</div>
+<?php 
+	$this->endWidget('zii.widgets.jui.CJuiDialog');
+?>
