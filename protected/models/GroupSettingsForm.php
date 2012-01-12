6a29cd0b000195937ae9263a8e0fcfad4471bbb6
adnankalay@gmail.com <adnankalay@gmail.com@06691718-432a-11de-86e1-e9dff1ea4737>
2012-01-12 22:15:36 +0000

    Committed for group settings (mostly user interface part) and some tooltips(delete, reject, approve).

---
+++ protected/models/GroupSettingsForm.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * GroupSettingsForm class.
+ * GroupSettingsForm is the data structure for adjusting the group settings such as adding users to groups 
+ * or deleting users from groups.
+ */
+class GroupSettingsForm extends CFormModel
+{
+	public $groupStatusArray;
+
+	/**
+	 * Declares the validation rules.
+	 * The rules state that username and password are required,
+	 * and password needs to be authenticated.
+	 */
+	public function rules()
+	{
+		return array(
+			array('groupStatusArray', 'boolean')
+		);
+	}
+
+	/**
+	 * Declares attribute labels.
+	 */
+	public function attributeLabels()
+	{
+		return array(
+		);
+	}
+}
