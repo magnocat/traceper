1a6b11166cace144429abdedf8508032ad4d9c30
adnankalay@gmail.com <adnankalay@gmail.com@06691718-432a-11de-86e1-e9dff1ea4737>
2012-02-11 12:36:42 +0000

    I18N for Turkish language support.

--- protected/views/users/userListDialog.php
+++ protected/views/users/userListDialog.php
@@ -3,7 +3,7 @@ $this->beginWidget('zii.widgets.jui.CJuiDialog', array(
 	    'id'=>'friendRequestsWindow',
 	    // additional javascript options for the dialog plugin
 	    'options'=>array(
-	        'title'=>Yii::t('general', 'Friend Requests'),
+	        'title'=>Yii::t('users', 'Friendship Requests'),
 	        'autoOpen'=>false,
 	        'modal'=>true, 
 			'resizable'=>false      
