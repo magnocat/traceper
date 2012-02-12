2d7aedaf88339d8ed7483046d02da867773aa4d8
Salman.Murat@gmail.com <Salman.Murat@gmail.com@06691718-432a-11de-86e1-e9dff1ea4737>
2012-02-12 11:50:08 +0000

    Geofence tables (traceper_geofence,traceper_geofence_user_relation) are updated.

--- branches/DevWebInterface/protected/models/Geofence.php
+++ branches/DevWebInterface/protected/models/Geofence.php
@@ -6,6 +6,8 @@
  * The followings are the available columns in table 'traceper_geofence':
  * @property integer $Id
  * @property integer $userId
+ * @property string $name
+ * @property string $description
  * @property string $point1Latitude
  * @property string $point1Longitude
  * @property string $point2Latitude
@@ -40,13 +42,15 @@ class Geofence extends CActiveRecord
 		// NOTE: you should only define rules for those attributes that
 		// will receive user inputs.
 		return array(
-			array('userId', 'required'),
+			array('userId, name', 'required'),
 			array('userId', 'numerical', 'integerOnly'=>true),
+			array('name', 'length', 'max'=>45),
+			array('description', 'length', 'max'=>500),
 			array('point1Latitude, point2Latitude, point3Latitude', 'length', 'max'=>8),
 			array('point1Longitude, point2Longitude, point3Longitude', 'length', 'max'=>9),
 			// The following rule is used by search().
 			// Please remove those attributes that should not be searched.
-			array('Id, userId, point1Latitude, point1Longitude, point2Latitude, point2Longitude, point3Latitude, point3Longitude', 'safe', 'on'=>'search'),
+			array('Id, userId, name, description, point1Latitude, point1Longitude, point2Latitude, point2Longitude, point3Latitude, point3Longitude', 'safe', 'on'=>'search'),
 		);
 	}
 
@@ -69,6 +73,8 @@ class Geofence extends CActiveRecord
 		return array(
 			'Id' => 'ID',
 			'userId' => 'User',
+			'name' => 'Name',
+			'description' => 'Description',
 			'point1Latitude' => 'Point1 Latitude',
 			'point1Longitude' => 'Point1 Longitude',
 			'point2Latitude' => 'Point2 Latitude',
@@ -91,6 +97,8 @@ class Geofence extends CActiveRecord
 
 		$criteria->compare('Id',$this->Id);
 		$criteria->compare('userId',$this->userId);
+		$criteria->compare('name',$this->name,true);
+		$criteria->compare('description',$this->description,true);
 		$criteria->compare('point1Latitude',$this->point1Latitude,true);
 		$criteria->compare('point1Longitude',$this->point1Longitude,true);
 		$criteria->compare('point2Latitude',$this->point2Latitude,true);
