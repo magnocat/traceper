6fff5d24481c99719cdf93f4ca79da2b822ed023
adnankalay@gmail.com <adnankalay@gmail.com@06691718-432a-11de-86e1-e9dff1ea4737>
2012-03-31 22:44:07 +0000

    Added for translations in UsersController.

---
+++ protected/messages/tr/users.php
@@ -0,0 +1,42 @@
+<?php 
+/**
+ * This file is the translation of files under views/users folder.
+ */
+
+/*
+ * Use the below corresponding Utf-8 characters, if you have problems with Turkish characters
+T�rk�e UTF-8
+� 	İ
+� 	ı
+� 	Ö
+� 	ö
+� 	Ü
+� 	ü
+� 	Ç
+� 	ç
+� 	ğ
+� 	ğ
+� 	�
+� 	ş
+*/
+return array(
+	'Search Results' => 'Arama Sonuçları',
+	'No users found' => 'Kullanıcı bulunamadı',
+	'Do you want to delete this user from your friend list?' => 'Bu kullanıyı arkadaş listenizden silmek istiyor musunuz?',
+	'Do you want to add this user as a friend?' => 'Bu kullanıyı arkadaş olarak eklemek istiyor musunuz?',	
+	'Do you want to reject this user\'s friend request?' => 'Bu kullanının arkadaşlık isteğini reddetmek istiyor musunuz?',
+	'No friendship requests found' => 'Arkadaşlık isteği bulunamadı',
+	'Sorry, an error occured in operation' => 'Üzgünüz, işlem sırasında bir hata oluştu',
+	'Friend request is sent' => 'Arkadaşlık isteği gönderildi',
+	'Group Settings' => 'Grup Ayarları',
+	'Geofence Settings' => 'Jeoçit Ayarları',
+	'Name' => 'İsim',
+	'No users to show...' => 'Kullanıcı bilgileri mevcut değil...',
+	'Name' => 'İsim',
+	'Name' => 'İsim',
+	'Name' => 'İsim',
+										
+);
+
+
+?>
