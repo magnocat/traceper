715e31f794aa8264a767facecff86281659d5e50
Salman.Murat@gmail.com <Salman.Murat@gmail.com@06691718-432a-11de-86e1-e9dff1ea4737>
2012-07-15 21:17:43 +0000

    Unit tests of PrivacyGroupsTest.php are completed

---
+++ branches/DevWebInterface/protected/tests/fixtures/traceper_privacy_groups.php
@@ -0,0 +1,24 @@
+<?php
+return array(
+		'privacyGroup1'=>array(
+				'Id'=>1,
+				'owner'=>5,
+				'type'=>0,
+				'name'=>'test1',
+				'description'=>'deneme1',
+		),
+		'privacyGroup2'=>array(
+				'Id'=>2,
+				'owner'=>5,
+				'type'=>1,
+				'name'=>'test2',
+				'description'=>'deneme2',
+		),
+		'privacyGroup3'=>array(
+				'Id'=>3,
+				'owner'=>6,
+				'type'=>0,
+				'name'=>'test3',
+				'description'=>'deneme3',
+		),
+);
--- branches/DevWebInterface/protected/tests/unit/PrivacyGroupsTest.php
+++ branches/DevWebInterface/protected/tests/unit/PrivacyGroupsTest.php
@@ -6,10 +6,9 @@ class PrivacyGroupsTest extends CDbTestCase
 {
 	
  	public $fixtures=array( 			
- 			'users'=>'Users',
+ 			'privacyGroups'=>'PrivacyGroups',
  			);
-	
-	
+		
 	public function testTableName()
 	{
 	    $this->assertEquals(PrivacyGroups::model()->tableName(),'traceper_privacy_groups');  	    
@@ -41,5 +40,43 @@ class PrivacyGroupsTest extends CDbTestCase
 		}
 	}
 	
-			
+	
+	public function testGetGroupsList()
+	{
+		//save groups for testing...
+		$this->assertTrue($this->privacyGroups("privacyGroup1")->save());
+		
+		$dataProvider = PrivacyGroups::model()->getGroupsList($this->privacyGroups("privacyGroup1")->owner, $this->privacyGroups("privacyGroup1")->type, 1);
+		$result=$dataProvider->getData();
+		$this->assertEquals($result[0]->name, $this->privacyGroups("privacyGroup1")->name);
+		
+		$this->assertTrue($this->privacyGroups("privacyGroup2")->save());
+		$dataProvider = PrivacyGroups::model()->getGroupsList($this->privacyGroups("privacyGroup2")->owner, $this->privacyGroups("privacyGroup2")->type, 1);
+		$result=$dataProvider->getData();
+		$this->assertEquals($result[0]->name, $this->privacyGroups("privacyGroup2")->name);
+	}
+	
+	public function testDeleteGroup()
+	{
+		//save groups for testing...
+		$this->assertTrue($this->privacyGroups("privacyGroup3")->save());	
+		$this->assertEquals(1,PrivacyGroups::model()->deleteGroup($this->privacyGroups("privacyGroup3")->id,$this->privacyGroups("privacyGroup3")->owner));
+		
+		//There is no group whose id is 5555
+		$this->assertEquals(-1,PrivacyGroups::model()->deleteGroup(5555,$this->privacyGroups("privacyGroup3")->owner));
+	}
+	
+	
+	public function testUpdatePrivacySettings()
+	{
+		$allowToSeeMyPosition = 0;
+		//save groups for testing...
+		$this->assertTrue($this->privacyGroups("privacyGroup3")->save());
+		
+		$this->assertEquals(1,PrivacyGroups::model()->updatePrivacySettings($this->privacyGroups("privacyGroup3")->id,$allowToSeeMyPosition));
+		
+		$rows = PrivacyGroups::model()->findAll("name=:name AND owner=:owner AND type=:type", array(":name"=>$this->privacyGroups("privacyGroup3")->name, "owner"=>$this->privacyGroups("privacyGroup3")->owner, "type"=>$this->privacyGroups("privacyGroup3")->type));
+		$this->assertEquals($allowToSeeMyPosition, $rows[0]->allowedToSeeOwnersPosition);
+	}
+	
 }
