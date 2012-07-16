5880e67a043c1ea999e68a4faf54b4fdeceab801
Salman.Murat@gmail.com <Salman.Murat@gmail.com@06691718-432a-11de-86e1-e9dff1ea4737>
2012-07-16 20:21:34 +0000

    Unit tests of UserPrivacyGroupRelationTest.php are completed

--- branches/DevWebInterface/protected/tests/fixtures/traceper_users.php
+++ branches/DevWebInterface/protected/tests/fixtures/traceper_users.php
@@ -1,15 +1,21 @@
 <?php
 return array(
-    'user1'=>array( 
-    	'Id'=>1,    
-    	'password'=>md5(12345),
-        'realname'=>'Test User',
-        'email'=>'test@traceper.com',
-    ),
-	'user2'=>array(
-		'Id'=>2,
-		'password'=>md5(54321),
-		'realname'=>'Test User 2',
-		'email'=>'test2@traceper.com',
-	),		
+		'user1'=>array(
+				'Id'=>1,
+				'password'=>md5(12345),
+				'realname'=>'Test User',
+				'email'=>'test@traceper.com',
+		),
+		'user2'=>array(
+				'Id'=>2,
+				'password'=>md5(54321),
+				'realname'=>'Test User 2',
+				'email'=>'test2@traceper.com',
+		),
+		'user_privacy_group_test'=>array(
+				'Id'=>3,
+				'password'=>md5(54321),
+				'realname'=>'User privacy group test',
+				'email'=>'user_privacy_group_test@traceper.com',
+		),
 );
