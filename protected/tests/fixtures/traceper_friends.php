6607ebffa6decb129260705c27bdb3dca12b11eb
adnankalay@gmail.com <adnankalay@gmail.com@06691718-432a-11de-86e1-e9dff1ea4737>
2012-07-17 11:15:45 +0000

    Test fixtures updated for new tests...

--- protected/tests/fixtures/traceper_friends.php
+++ protected/tests/fixtures/traceper_friends.php
@@ -1,4 +1,30 @@
 <?php
 //For empty friends table
-return array(		
+return array(	
+		'friendship1'=>array(
+				'Id'=>1,
+				'friend1'=>1,
+				'friend2'=>2,
+				'status'=>1,
+		),	
+
+		'friendship2'=>array(
+				'Id'=>2,
+				'friend1'=>1,
+				'friend2'=>3,
+				'status'=>1,
+		),
+
+		'friendship3'=>array(
+				'Id'=>3,
+				'friend1'=>1,
+				'friend2'=>4,
+				'status'=>1,
+		),		
+		'friendship4'=>array(
+				'Id'=>4,
+				'friend1'=>1,
+				'friend2'=>5,
+				'status'=>1,
+		),		
 );
--- protected/tests/fixtures/traceper_users.php
+++ protected/tests/fixtures/traceper_users.php
@@ -5,17 +5,34 @@ return array(
 				'password'=>md5(12345),
 				'realname'=>'Test User',
 				'email'=>'test@traceper.com',
+				'userType' => UserType::RealUser
 		),
 		'user2'=>array(
 				'Id'=>2,
 				'password'=>md5(54321),
 				'realname'=>'Test User 2',
 				'email'=>'test2@traceper.com',
+				'userType' => UserType::GPSDevice
 		),
 		'user_privacy_group_test'=>array(
 				'Id'=>3,
 				'password'=>md5(54321),
 				'realname'=>'User privacy group test',
 				'email'=>'user_privacy_group_test@traceper.com',
+				'userType' => UserType::RealStaff
 		),
+		'user4'=>array(
+				'Id'=>4,
+				'password'=>md5(12321),
+				'realname'=>'Test User 4',
+				'email'=>'test4@traceper.com',
+				'userType' => UserType::GPSStaff
+		),	
+		'user5'=>array(
+				'Id'=>5,
+				'password'=>md5(1234321),
+				'realname'=>'Test User 5',
+				'email'=>'test5@traceper.com',
+				'userType' => UserType::RealUser
+		),			
 );
